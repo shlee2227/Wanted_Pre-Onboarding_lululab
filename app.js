1898a0f49cce88bf07df2958a3b66e85f2732f99
Lee Shinhee <shlee2227@gmail.com>
2022-10-17 10:53:10 +0900

    [ADD] 초기 세팅 추가

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes/index");
+const morgan = require("morgan");
+
+const createApp = () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(router);
+  app.use(morgan("combined"));
+
+  app.get("/ping", function (req, res, next) {
+    res.json({ message: "pong" });
+  });
+
+  return app;
+};
+
+module.exports = { createApp };
---
+++ services/user.js
@@ -0,0 +1,7 @@
+const userDao = require("../models/user");
+
+const func = async () => {};
+
+module.exports = {
+  func,
+};
